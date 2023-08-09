7a78a99b64feef40071557aa2ff58f9207131c05
BoburAzimov2020 <azimov.b2020@gmail.com>
2023-08-09 13:48:10 +0500

    ficha-002: test-2

--- src/main/java/com/onevizion/crudofbook/controller/BookControllerImpl.java
+++ src/main/java/com/onevizion/crudofbook/controller/BookControllerImpl.java
@@ -42,6 +42,7 @@ public class BookControllerImpl implements BookController {
         return bookService.getTopAuthors(character);
     }
 
+    // TEST-003
     @Override
     public ApiResult<Book> getFirst() {
         // TODO Auto-generated method stub - Some additional text for test.
